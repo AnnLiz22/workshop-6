5c6e4d73c5092aeded9c3dfc8f92c55abf72eecf
AnnLiz22 <bielakam@gmail.com>
2024-06-12 23:00:09 +0200

    appConfig added; beans for : entity manager factory, transaction manager

---
+++ src/main/java/pl/coderslab/AppConfig.java
@@ -0,0 +1,54 @@
+package pl.coderslab;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.persistence.EntityManagerFactory;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Configuration
+@ComponentScan(basePackages = "pl.coderslab")
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableJpaRepositories
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        StringHttpMessageConverter stringConverter = new StringHttpMessageConverter();
+        stringConverter.setSupportedMediaTypes(List.of(new MediaType("text", "plain",
+                StandardCharsets.UTF_8)));
+        converters.add(stringConverter);
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+        emfb.setPersistenceUnitName("workshopHibernate");
+        return emfb;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+}
