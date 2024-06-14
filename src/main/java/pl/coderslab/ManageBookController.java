26ba8aa99ccd320e0b16508e6db5e38b9c6e855a
AnnLiz22 <bielakam@gmail.com>
2024-06-14 15:36:39 +0200

    controller for managing books

--- src/main/java/pl/coderslab/ManageBookController.java
+++ src/main/java/pl/coderslab/ManageBookController.java
@@ -1,16 +1,27 @@
 package pl.coderslab;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
+@Slf4j
 @RequestMapping("/admin/books")
+@RequiredArgsConstructor
 public class ManageBookController {
 
-    private final BookService bookService;
+    private final JpaBookService jpaBookService;
 
-    public ManageBookController(BookService bookService) {
-        this.bookService = bookService;
+    @GetMapping
+    public String showPosts(Model model) {
+        List<Book> books = jpaBookService.getBooks();
+        model.addAttribute("books", books);
+        return "bookList";
     }
 
 }
