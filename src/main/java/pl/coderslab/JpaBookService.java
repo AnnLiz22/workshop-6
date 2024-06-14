020aef16daec4e2f4e6542e1acfa20ae36bd6c84
AnnLiz22 <bielakam@gmail.com>
2024-06-15 00:01:19 +0200

    service implementation completed

--- src/main/java/pl/coderslab/JpaBookService.java
+++ src/main/java/pl/coderslab/JpaBookService.java
@@ -16,23 +16,27 @@ public class JpaBookService implements BookService {
 
     @Override
     public List<Book> getBooks() {
-        return null;
+        return bookRepository.findAll();
     }
 
     @Override
     public Optional<Book> get(Long id) {
-        return Optional.empty();
+       Optional<Book> book = bookRepository.getBookById(id);
+       return book;
     }
 
     @Override
     public void add(Book book) {
+        bookRepository.insert(book);
     }
 
     @Override
     public void delete(Long id) {
+        bookRepository.removeBookById(id);
     }
 
     @Override
     public void update(Book book) {
+        bookRepository.update(book);
     }
 }
