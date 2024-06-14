9685a8886e4340e46142fe4ce9add6d75a3399a4
AnnLiz22 <bielakam@gmail.com>
2024-06-14 23:59:22 +0200

    methods added to repository

--- src/main/java/pl/coderslab/BookRepository.java
+++ src/main/java/pl/coderslab/BookRepository.java
@@ -1,14 +1,30 @@
 package pl.coderslab;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("select b from Book b where b.id = ?1")
-    List<Book> getBookById(Long id);
+    Optional<Book> getBookById(Long id);
 
+    @Query("select b from Book b")
+    List<Book> findAll();
 
+    @Modifying
+    @Query(value = "insert into books (isbn, title, author, publisher, type) values (?, ?, ?, ?, ?)", nativeQuery = true)
+    void insert(Book book);
+
+    @Query("delete from Book b where b.id = ?1")
+    Optional<Book> removeBookById(Long id);
+
+    @Modifying
+    @Query(value = "update books set isbn = ?", nativeQuery = true)
+    void update(Book book);
 }
