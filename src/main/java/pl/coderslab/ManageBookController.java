bdc778813cdaace064a9a397318c3c3b8ae3957a
AnnLiz22 <bielakam@gmail.com>
2024-06-15 08:49:51 +0200

    actions for editing, deleting, getting all added to controller; model attribute created

--- src/main/java/pl/coderslab/ManageBookController.java
+++ src/main/java/pl/coderslab/ManageBookController.java
@@ -1,16 +1,15 @@
 package pl.coderslab;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -39,8 +38,31 @@ public class ManageBookController {
             jpaBookService.update(book);
             log.info("Updated {}", book);
         }
-            return "redirect:/admin/books";
-        }
+        return "redirect:/admin/books";
+    }
 
+    @PostMapping("/edit/{id}")
+    public String editBook(@PathVariable Long id, Model model){
+        Optional<Book> book = jpaBookService.get(id);
+        model.addAttribute("book", book);
+        return "bookForm";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id){
+        jpaBookService.delete(id);
+        log.info("Deleted book with id {}", id);
+        return "redirect:/admin/books/all";
+    }
+
+    @GetMapping("/all")
+    public String showAllBooks(){
+        return "bookList";
+    }
+
+    @ModelAttribute("books")
+    public List<Book> getAllBooks(){
+        return jpaBookService.getBooks();
+    }
 
 }
